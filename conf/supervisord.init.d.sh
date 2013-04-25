14c55011afe1b80ac635fb03f9c4de2b5747c469
Mahmoud Hashemi <mahmoudrhashemi@gmail.com>
2013-04-24 23:23:35 -0700

    some formatting

--- conf/supervisord.init.d.sh
+++ conf/supervisord.init.d.sh
@@ -19,8 +19,18 @@
 # source function library
 . /etc/rc.d/init.d/functions
 
-# source system settings
-[ -e /etc/sysconfig/supervisord ] && . /etc/sysconfig/supervisord
+set -a 
+
+# should probably put both of these options as runtime arguments 
+OPTIONS="-c /etc/supervisord.conf"
+PIDFILE=/var/run/supervisord.pid
+
+# unset this variable if you don't care to wait for child processes to shutdown before removing the /var/lock/subsys/supervisord lock
+WAIT_FOR_SUBPROCESSES=yes
+
+# remove this if you manage number of open files in some other fashion
+ulimit -n 96000
+
 
 RETVAL=0
 
@@ -44,26 +54,27 @@ start() {
 stop() {
     echo -n "Stopping supervisord: "
     /usr/local/bin/supervisorctl $OPTIONS shutdown
-	if [ -n "$WAIT_FOR_SUBPROCESSES" ]; then 
-        echo "Waiting roughly 60 seconds for $PIDFILE to be removed after child processes exit"
-        for sleep in  2 2 2 2 4 4 4 4 8 8 8 8 last; do
-            if [ ! -e $PIDFILE ] ; then
-                echo "Supervisord exited as expected in under $total_sleep seconds"
-                break
+    if [ -n "$WAIT_FOR_SUBPROCESSES" ]; then 
+    echo "Waiting roughly 60 seconds for $PIDFILE to be removed after child processes exit"
+    for sleep in  2 2 2 2 4 4 4 4 8 8 8 8 last; do
+        if [ ! -e $PIDFILE ] ; then
+            echo "Supervisord exited as expected in under $total_sleep seconds"
+            break
+        else
+            if [[ $sleep -eq "last" ]] ; then
+                echo "Supervisord still working on shutting down. We've waited roughly 60 seconds, we'll let it do its thing from here"
+                return 1
             else
-                if [[ $sleep -eq "last" ]] ; then
-                    echo "Supervisord still working on shutting down. We've waited roughly 60 seconds, we'll let it do its thing from here"
-                    return 1
-                else
-                    sleep $sleep
-                    total_sleep=$(( $total_sleep + $sleep ))
-                fi
-
+                sleep $sleep
+                total_sleep=$(( $total_sleep + $sleep ))
             fi
-        done
+	    
+        fi
+    done
     fi
 
-    # always remove the subsys.  we might have waited a while, but just remove it at this point.
+    # always remove the subsys.  we might have waited a while, but
+    # just remove it at this point.
     rm -f /var/lock/subsys/supervisord
 }
 
